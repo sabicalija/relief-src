770fe0a8771e322a62ce4c56c9f0638f76ab26ae
Alija Sabic <sabic.alija@gmail.com>
2023-05-23 23:04:12 +0200

    Remove template code and component, add SubmitUrl and UploadImage component

---
+++ src/components/SubmitUrl.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const imageUrl = ref("");
+
+const submitUrl = () => {
+  console.log(imageUrl.value);
+};
+</script>
+
+<template>
+  <form @submit.prevent="submitUrl">
+    <input type="text" v-model="imageUrl" placeholder="Enter image URL" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
---
+++ src/components/UploadImage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const image = ref("");
+
+const onFileChange = (e: Event) => {
+  const files = (e.target as HTMLInputElement).files;
+  if (!files || files.length === 0) return;
+  createImage(files[0]);
+};
+
+const createImage = (file: File) => {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    image.value = e.target?.result as string;
+  };
+  reader.readAsDataURL(file);
+};
+</script>
+
+<template>
+  <input type="file" accept="image/*" @change="onFileChange" />
+</template>
